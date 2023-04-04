ec949c1e0eee3c486a5610b5e537304737bb68a2
Lavanyabalu <lavanyabalu>
2023-04-04 22:52:44 +0530

    git one

---
+++ src/org/hcl/EEmployee.java
@@ -0,0 +1,32 @@
+package org.hcl;
+public class EEmployee {
+	public void empId() {
+System.out.println("My employee id:123");
+}
+public void empName() {
+ System.out.println("My employee name:Ramu");
+}
+public void empDob() {
+System.out.println("My employee Dob:12.9.1992");
+}
+public void empphone() {
+System.out.println("My employee phone:1234567890");
+}
+public void empEmail() {
+System.out.println("My Employee Email:adb@gmail.com");
+}
+public void empAddress() {
+System.out.println("My Employee Address:east street");
+}
+public static void main(String[] args) {
+	EEmployee P=new EEmployee();
+	P.empId();
+	P.empName();
+	P.empDob();
+	P.empphone();
+	P.empEmail();
+	P.empAddress();
+	
+	
+}
+}
---
+++ src/org/hcl/Employee.java
@@ -0,0 +1,18 @@
+package org.hcl;
+
+public class Employee {
+	public void empId() {
+
+		System.out.println("My employee id:235");
+	}
+public void empName() {
+System.out.println("My employee name:Lavanya");
+}
+
+public static void main(String[] args) {
+	Employee B=new Employee();
+	B.empId();
+	B.empName();
+	
+}
+}
